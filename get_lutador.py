ff65c3c87505805bbfbc30dea9a4035c1b9b08a7
Felipe Costa de Sousa <felipecostadesousa.sigma@gmail.com>
2025-06-01 15:14:04 -0300

    Melhorias

--- get_lutador.py
+++ get_lutador.py
@@ -2,23 +2,23 @@ import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def get_lutador(firstName, middleName, lastName):
+def get_lutador(firstName, middleName, lastName, x_rapidapi_key):
   url = "https://ufc_fighter_stats.p.rapidapi.com/fighter"
   querystring = {"firstName": firstName, "middleName": middleName, "lastName": lastName}
-  headers = {"x-rapidapi-key": "7b4ef99412msha2707ae4be4d7a4p15fc0ajsnf0cd1f224e20","x-rapidapi-host": "ufc_fighter_stats.p.rapidapi.com"}
+  headers = {"x-rapidapi-key": x_rapidapi_key,"x-rapidapi-host": "ufc_fighter_stats.p.rapidapi.com"}
   response = requests.get(url, headers=headers, params=querystring, verify=False)
   nome = " ".join(part for part in [firstName, middleName, lastName] if part)
   if response.status_code == 200:
     if(response.json()["record"]["losses"] == 0):
       if(not((nome == 'Alberto Montes') or (nome == 'Islam Dulatov') or (nome == 'Kevin Christian') or (nome == 'Seokhyeon Ko'))):
-        print(f"✅ {nome} ({response.json()['record']['wins']})")
-        f = open("/home/felipe-costa/Documentos/Python/invictos.txt", "a")
+        print(f"✅ {nome} ({response.json()['record']['wins']}-0)")
+        f = open("invictos.txt", "a")
         if(nome == "DongHun Choi"):
           f.write(f"{nome} (9)\n")
         else:
           f.write(f"{nome} ({response.json()['record']['wins']})\n")
         f.close()
     else:
-      print(f"❌ {nome} ({response.json()['record']['wins']} - {response.json()['record']['losses']})")
+      print(f"❌ {nome} ({response.json()['record']['wins']}-{response.json()['record']['losses']})")
   else:
-    print(f"⚠️ {nome} (ERRO)")
+    print(f"❔ {nome} (ERRO)")
--- leitura_lutadores.py
+++ leitura_lutadores.py
@@ -1,14 +1,15 @@
 from get_lutador import get_lutador
 
 def leitura_lutadores():
+  x_rapidapi_key = ''
   with open("lutadores.txt") as lutadores:
     arquivo = open("invictos.txt", "w")
     arquivo.close()
     for lutador in lutadores:
       nome = lutador.strip().split()
       if(len(nome) == 1):
-        get_lutador(nome[0], '', '')
+        get_lutador(nome[0], '', '', x_rapidapi_key)
       if(len(nome) == 2):
-        get_lutador(nome[0], '', nome[1])
+        get_lutador(nome[0], '', nome[1], x_rapidapi_key)
       elif(len(nome) == 3):
-        get_lutador(nome[0], nome[1], nome[2])
+        get_lutador(nome[0], nome[1], nome[2], x_rapidapi_key)
